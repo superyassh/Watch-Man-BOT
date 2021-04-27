090b9591572e81ee014037bafee3737814e34fd0
Pavitra Bhagat <pavbyte@gmail.com>
2021-04-27 20:51:54 +0000

    branch

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive"
+    return "<H1> Minor Project </H1> <p><BR> Discord BOT is online </p><BR> Created by @superyassh & Team "
 
 def run():
   app.run(host='0.0.0.0',port=8080)
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import discord
 import time
 import asyncio
-import re
+from googleapiclient import discovery
+import json
+import aiohttp
 import random
 
 from keep_alive import keep_alive
-
+intents = discord.Intents.default()
+intents.members=True
+client = discord.Client(intents=intents)
 intents = discord.Intents(messages=True, guilds=True, members=True)
 
-from settings import *
 
 messages = joined = 0
+
 
 def read_token():
     with open("token.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
 
+
 token = read_token()
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('Logged in...')
     print('Username: ' + str(client.user.name))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='SCAM 1992'))
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name='Over You'))
+
 
 async def update_stats():
     await client.wait_until_ready()
@@ -34,7 +42,9 @@ async def update_stats():
     while not client.is_closed():
         try:
             with open("stats.txt", "a") as f:
-                f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")
+                f.write(
+                    f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n"
+                )
 
             messages = 0
             joined = 0
@@ -44,13 +54,13 @@ async def update_stats():
             print(e)
             await asyncio.sleep(5)
 
+
 @client.event
-async def on_member_join(member):
-    global joined
-    joined += 1
-    for channel in member.server.channels:
-        if str(channel) == "welcome":
-            await channel.send(f"""Welcome to the server {member.mention}""")
+async def on_message_join(member):
+    channel = client.get_channel(channel_id)
+    embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!") # F-Strings!
+    embed.set_thumbnail(url=member.avatar_url) # Set the embed's thumbnail to the member's avatar image!
+    await channel.send(embed=embed)
 
 
 @client.event
@@ -60,24 +70,55 @@ async def on_message(message):
     messages += 1
     id = client.get_guild(784161461244395570)
     channels = ["commands"]
-    valid_users = ["TUDU#4367", "TuDu#5206"]
-    bad_words = ["bad", "stop", "dog"]
+    valid_users = ["TUDU#4367", "TuDu#5206", "0xKage#5206"]
 
-#message filter
-    if(message.author.id != client.user.id):
-        for word in bad_words:
-            if message.content.count(word) > 0:
-                username = message.author.name
-                userID = message.author.id
-                print("%s has used the Bad Word." % (username))
-                await message.channel.purge(limit=1)
-                embed = discord.Embed(
-                description=" <@%s> don't use banned words." % (userID),
-                color=0x00FF00
-            )
-                await message.channel.send(content=None, embed=embed)
-            
-#help            
+    if (message.author.id != client.user.id):
+        API_KEY = 'AIzaSyCq17LT42MhHnGO0MIwSymviKVz3m9vFMI'
+        clientGoogle = discovery.build(
+            "commentanalyzer",
+            "v1alpha1",
+            developerKey=API_KEY,
+            discoveryServiceUrl=
+            "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+            static_discovery=False,
+        )
+        analyze_request = {
+            'comment': {
+                'text': message.content
+            },
+            'requestedAttributes': {
+                'TOXICITY': {}
+            }
+        }
+        response = clientGoogle.comments().analyze(
+            body=analyze_request).execute()
+        # response_dict=json.loads(response.content)
+        results = json.dumps(response, indent=2)
+        json_results = json.loads(results)
+        toxicity = json_results['attributeScores']['TOXICITY']['summaryScore'][
+            'value']
+        #print(json.dumps(response,indent=2))
+        #print(toxicity)
+        toxicity_percentage = toxicity*100
+        if toxicity > 0.9:
+            username = message.author.name
+            userID = message.author.id
+            print("%s has used the Bad Word which has toxicity is %s" %
+                  (username, toxicity))
+            await message.channel.purge(limit=1)
+            embed = discord.Embed(
+                description=
+                " <@%s> don't use banned words.\n\n Toxicity in your Message is %.2f %%"
+                % (userID, toxicity_percentage),
+                color=0x00FF00)
+            await message.channel.send(content=None, embed=embed)
+            await message.author.send(content=None, embed=embed)
+            with open("data.txt", "a") as f:
+                        f.write(
+                            f"%s has used the Bad Word which has toxicity level %s\n"
+                            % (username, toxicity))
+
+#help
     if message.content == "help":
         embed = discord.Embed(title="BOT Help", description="Commands")
         embed.add_field(name="hello", value="Greets the user")
@@ -87,22 +128,44 @@ async def on_message(message):
 
 #hello
     if message.content.find("hello") != -1:
-            await message.channel.send("Hi there!") 
+        await message.channel.send("Hi there!")
+
+#hello
+    if message.content == "!users":
+        await message.channel.send(f"""Numbers of Members: {id.member_count}"""
+                                   )
+
 
 #shutdown
-    if str(message.channel) in channels and str(message.author) in valid_users:
-        if message.content.find("!shutdown") != -1:
-            embed = discord.Embed(
-                description="Shutting down. Bye! :wave:",
-                color=0x00FF00
-            )
-            await message.channel.send(content=None, embed=embed)
-            await client.logout()
-            await client.close()
-        if message.content == "!users":
-            await message.channel.send(f"""Numbers of Members: {id.member_count}""")
+    if message.content.find(
+            "!shutdown") != -1 and message.author in valid_users:
+        embed = discord.Embed(description="Shutting down. Bye! :wave:",
+                              color=0x00FF00)
+        await message.channel.send(content=None, embed=embed)
+        await client.logout()
+        await client.close()
 
+    if message.content == "!cats":
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://aws.random.cat/meow') as r:
+                res = await r.json()
+                emojis = [':cat2: ', ':cat: ', ':heart_eyes_cat: ']
+                await message.channel.send(random.choice(emojis) + res['file'])
+
+    if message.content == "!dogs":
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://dog.ceo/api/breeds/image/random') as r:
+                res = await r.json()
+                emojis = [':dog: ', ':dog2: ']
+                await message.channel.send(random.choice(emojis) + res['message'])
+
+    if message.content == "!countdown":
+        countdown = ['five', 'four', 'three', 'two', 'one']
+        for num in countdown:
+            await message.channel.send('**:{0}:**'.format(num))
+            await asyncio.sleep(1)
+        await message.channel.send('**:ok:** BOOM')
 
 keep_alive()
 client.loop.create_task(update_stats())
-client.run(token) 
+client.run(token)
